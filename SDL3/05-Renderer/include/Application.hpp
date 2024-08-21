c87b646612b0f438ae10209373d328a2293893bb
Harsha G D <harshagd695@gmail.com>
2024-08-21 19:50:18 +0530

    05-Renderer SDL3

---
+++ SDL3/05-Renderer/include/Application.hpp
@@ -0,0 +1,91 @@
+#pragma once 
+
+#include <SDL3/SDL.h>
+
+struct Application{
+	Application(int argc, char **argv){
+		StartUp();
+	}
+	~Application(){
+		ShutDown();
+	}
+
+	void ShutDown(){
+		SDL_DestroyRenderer(mRenderer);
+		SDL_DestroyWindow(mWindow);
+		SDL_Quit();
+	}
+
+	void StartUp(){
+		if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0){
+			SDL_Log("Error %s",SDL_GetError());
+		}
+		mWindow = SDL_CreateWindow("SDL3", 640, 480, SDL_EVENT_WINDOW_SHOWN);
+
+		mRenderer = SDL_CreateRenderer(mWindow, NULL);
+		if(mRenderer == nullptr){
+			SDL_Log("Error with Renderer");
+		}
+		
+		mRect.x = 5.0f;
+		mRect.y = 5.0f;
+		mXVelocity = 5.0f;
+		mYVelocity = 5.0f;
+	
+	}
+
+	void Update(){
+
+		mRect.x += mXVelocity;
+		mRect.y += mYVelocity;
+
+		if(mRect.x + mRect.w  > 640 || mRect.x < 0){
+			mXVelocity = -mXVelocity;		
+		}
+		if(mRect.y + mRect.h > 480 || mRect.y < 0){
+			mYVelocity = -mYVelocity;
+		}
+		
+	}
+
+	void Render(){
+
+		SDL_SetRenderDrawColor(mRenderer, 0xff, 0, 0, SDL_ALPHA_OPAQUE);
+		SDL_RenderClear(mRenderer);
+	
+		SDL_SetRenderDrawColor(mRenderer, 0, 0xff, 0, SDL_ALPHA_OPAQUE);	
+		SDL_RenderRect(mRenderer, &mRect);
+		
+	
+		SDL_RenderPresent(mRenderer);
+	
+	}
+
+	void Input(){
+		SDL_Event event;
+		if(SDL_PollEvent(&event)){
+			if(event.type == SDL_EVENT_QUIT){
+				mRun = false;
+			}
+		}
+
+	}
+
+	void Loop(){
+		while(mRun){
+			Input();
+			Update();
+			Render();
+			SDL_Delay(16);
+		}
+	}
+
+	private:
+		bool mRun{true};
+		SDL_Window *mWindow;
+		SDL_Renderer *mRenderer;
+		SDL_FRect mRect{0.0f, 0.0f, 40.0f, 20.0f};
+		float mXVelocity;
+		float mYVelocity;
+
+};
